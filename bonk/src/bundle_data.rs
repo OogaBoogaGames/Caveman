319df95ab8b2e30440a1468834202041db97f4fd
endercass <46408156+Endercass@users.noreply.github.com>
2023-08-16 16:56:04 -0400

    bonk utility basic setup

---
+++ bonk/src/bundle_data.rs
@@ -0,0 +1,17 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct BundleManifest {
+    pub title: String,
+    pub description: String,
+    pub provides: String,
+    pub assets: Vec<AssetDescriptor>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AssetDescriptor {
+    pub token: String,
+    pub compress: bool,
+    pub mime_type: String,
+    pub path: String
+}
--- bonk/src/main.rs
+++ bonk/src/main.rs
@@ -1,3 +1,33 @@
+mod bundle_data;
+use std::fs::File;
+use caveman::proto::Caveman::CavemanBundle;
+
+use clap::{arg, Parser, ValueHint};
+
+use crate::bundle_data::BundleManifest;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = "Hii")]
+struct Arguments {
+    #[arg(value_name = "bundle", value_hint = ValueHint::DirPath, help = "Bundle manifest to read")]
+    manifest: std::path::PathBuf,
+    #[arg(short = 'o', long = "out", value_name = "output", value_hint = ValueHint::DirPath, help = "The name of the output bundle")]
+    out: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    println!("Reading {}", args.manifest.display());
+
+    let manifest_file = File::open(args.manifest).unwrap();
+
+    let manifest: BundleManifest = serde_json::from_reader(manifest_file).unwrap();
+
+    let mut bundle = CavemanBundle::new();
+
+    bundle.title = manifest.title;
+
+    println!("{}", bundle)
 }
